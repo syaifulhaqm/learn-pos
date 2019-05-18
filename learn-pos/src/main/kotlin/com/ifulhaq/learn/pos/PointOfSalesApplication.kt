75550b6c5574270653b2e04239271f4e2a312d12
syaifulhaqm <37460594+syaifulhaqm@users.noreply.github.com>
2019-05-18 09:47:19 +0700

    first commit

---
+++ learn-pos/src/main/kotlin/com/ifulhaq/learn/pos/PointOfSalesApplication.kt
@@ -0,0 +1,11 @@
+package com.ifulhaq.learn.pos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PointOfSalesApplication
+
+fun main(args: Array<String>) {
+	runApplication<PointOfSalesApplication>(*args)
+}
---
+++ learn-pos/src/test/kotlin/com/ifulhaq/learn/pos/PointOfSalesApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.ifulhaq.learn.pos
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class PointOfSalesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
